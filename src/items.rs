08a5bb7d74f69a5351d79e208ac09ee84a4adba4
Emilly M.S. <soulinity.h@gmail.com>
2023-11-06 14:33:25 -0300

    Finished append_to_item function

--- src/items.rs
+++ src/items.rs
@@ -25,8 +25,9 @@ pub fn add_item(path: &str, item: &str) {
 
 pub fn delete_item(path: &str, id: u8) {
     // {{{
-    // Scope ensures files are closed
     let out_path = path.to_string() + ".tmp";
+
+    // Scope ensures files are closed
     {
         let file = File::open(path).expect("Unable to open list for reading");
         let out_file = File::create(&out_path).expect("Unable to create output file");
@@ -45,11 +46,41 @@ pub fn delete_item(path: &str, id: u8) {
 }
 // }}}
 
-pub fn edit_item(path: &str, id: u8, new_content: &str) {}
+pub fn edit_item(path: &str, id: u8, new_content: &str) {
+    // {{{
+}
+// }}}
 
-pub fn append_to_item(path: &str, id: u8, content: &str) {}
+pub fn append_to_item(path: &str, id: u8, content: &str) {
+    // {{{
+    crate::check_list(path);
+    let out_path = path.to_string() + ".tmp";
 
-pub fn move_item(path: &str, from: u8, to: u8) {}
+    // Scope ensures files are closed
+    {
+        let file = File::open(path).expect("Unable to open list for reading");
+        let out_file = File::create(&out_path).expect("Unable to create output file");
+
+        let reader = BufReader::new(&file);
+        let mut writer = BufWriter::new(&out_file);
+        let id = id - 1;
+
+        for (i, ln) in reader.lines().map(|l| l.unwrap()).enumerate() {
+            if i != id.into() {
+                writeln!(writer, "{ln}").expect("Unable to write to tmp file");
+            } else {
+                writeln!(writer, "{ln}{content}").expect("Unable to write to tmp file");
+            }
+        }
+    }
+    fs::rename(out_path, path).expect("Unable to rename tmp file");
+}
+// }}}
+
+pub fn move_item(path: &str, from: u8, to: u8) {
+    // {{{
+}
+// }}}
 
 #[cfg(test)]
 mod test {
