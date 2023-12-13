f9e2b3f875785929f85dad84ad2b29471864c6e4
Emilly M.S. <soulinity.h@gmail.com>
2023-12-13 15:40:11 -0300

    Improve bash completions
    Now the old item that will be edited will be auto-completed when pressing <tab> after passing the
    id.

--- tsk.sh
+++ tsk.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
 
-complete -W "--help --version --no-color print do undo clear add append edit move delete" tsk
+_tsk_comp() {
+    if [[ ${#COMP_WORDS[@]} != 2 && ${COMP_WORDS[1]} != "edit" ]] ;then
+        return
+    fi
+    local comps=( $(compgen -W "--help --version --no-color print do undo clear add append edit move delete clear" -- "${COMP_WORDS[1]}") )
+
+    if [[ ${COMP_WORDS[1]} == "edit" && ${#COMP_WORDS[@]} == 4 ]]; then
+        local old_ifs="$IFS"
+        IFS=$'\n'
+
+        local items=( $(< ~/.local/share/tsk/task_list)  )
+        local id=$(( ${COMP_WORDS[2]} - 1 ))
+
+        if [[ $id -lt 0 ]]; then
+            IFS="$old_ifs"
+            return
+        fi
+
+        local item="${items[$id]}"
+        local item="${item/\[*\]/}"
+
+        COMPREPLY=( "'$(compgen -W "$item" -- "${COMP_WORDS[3]}")'" )
+        IFS="$old_ifs"
+    elif [[ ${COMP_WORDS[1]} != "edit" ]]; then
+        COMPREPLY=("${comps[@]}")
+    fi
+}
+
+complete -F _tsk_comp tsk
